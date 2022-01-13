0059880d2f7b0fc7180b39661bc941bc84492f7e
alexei0960 <alexei0960@yandex.ru>
2022-01-13 13:20:11 +0400

    Create Figure.cs

---
+++ Figure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Figure
+    {
+        public double GetSquare(double r)
+        {
+            return 3.14 * r * r;
+        }
+        public double GetSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+    }
+}
